7b0a06158ba6020a07ace785f1fc64577ad1e424
GodReorx <eduard.granero@gmail.com>
2024-04-13 11:28:56 +0200

    Creacion estructura

---
+++ src/main/java/cat/itacademy/barcelonactiva/granero/eduard/s05/t01/n02/S05T01N02GraneroEduard/S05T01N02GraneroEduardApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.granero.eduard.s05.t01.n02.S05T01N02GraneroEduard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N02GraneroEduardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N02GraneroEduardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/granero/eduard/s05/t01/n02/S05T01N02GraneroEduard/S05T01N02GraneroEduardApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.granero.eduard.s05.t01.n02.S05T01N02GraneroEduard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N02GraneroEduardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
